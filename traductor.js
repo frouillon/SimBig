e848de6c1d1ab92abd00b5b123ad1f0c8bce7340
Andres <andreus.morx@gmail.com>
2025-06-29 11:25:48 -0500

    11:00AM

---
+++ traductor.js
@@ -0,0 +1,40 @@
+const translations = {
+    es: {
+        home: "Inicio",
+        information: "Información",
+        speakers: "Ponentes",
+        syllabus: "Temario",
+        courses: "Cursos",
+        schedule: "Horario",
+        sponsors: "Patrocinadores",
+        dates: "Fechas",
+        faq: "Preguntas Frecuentes",
+        contact: "Contacto",
+        language: "🇪🇸 ESPAÑOL ▼",
+        // Hero
+        title: "Escuela LATAM de<br />Inteligencia Artificial<br />2025",
+        colocated: "Coorganizado con SIMBig 2025",
+        location: "📍 FISI-UNMSM, Lima, Perú",
+        date: "📅 29–31 Octubre 2025",
+        timer: "TEMPORIZADOR 99 días &nbsp; 24h &nbsp; 60 min &nbsp; 60 segundos"
+    },
+    en: {
+        home: "Home",
+        information: "Information",
+        speakers: "Speakers",
+        syllabus: "Syllabus",
+        courses: "Courses",
+        schedule: "Schedule",
+        sponsors: "Sponsors",
+        dates: "Dates",
+        faq: "FAQ",
+        contact: "Contact",
+        language: "🇬🇧 ENGLISH ▼",
+        // Hero
+        title: "LATAM School of<br />Artificial Intelligence<br />2025",
+        colocated: "Co-located with SIMBig 2025",
+        location: "📍 FISI-UNMSM, Lima, Perú",
+        date: "📅 29–31 October 2025",
+        timer: "TIMER 99 days &nbsp; 24h &nbsp; 60 min &nbsp; 60 seconds"
+    }
+};
